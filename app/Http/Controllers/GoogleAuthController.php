0b53de82320a56893a0c90ae241f79aef4095f81
rMi99 <99ramigimhama@gmail.com>
2023-10-10 09:15:19 +0530

    change google auth

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -3,37 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleAuthController extends Controller
 {
+
     public function redirectToGoogle()
     {
         return Socialite::driver('google')->redirect();
     }
 
+
     public function handleGoogleCallback()
     {
-        try {
-            $google_user = Socialite::driver('google')->user();
-            $user = User::where('google_id', $google_user->getId())->first();
 
-            if (!$user) {
-                $new_user = User::create([
-                    'name' => $google_user->getName(),
-                    'email' => $google_user->getEmail(),
-                    'google_id' => $google_user->getId(),
-                ]);
+try {
 
-                Auth::login($new_user);
-                return redirect()->intended('home');
-            } else {
-                Auth::login($user);
-                return redirect()->intended('home');
-            }
-        } catch (\Exception $e) {
-            return redirect()->route('login')->with('error', 'Google login failed');
-        }
+    $user = Socialite::driver('google')->user();
+    $finduser = User::where('google_id', $user->id)->first();
+
+    if($finduser){
+
+        Auth::login($finduser);
+
+        return redirect()->intended('dashboard');
+
+    }else{
+
+        $newUser = User::updateOrCreate(['email' => $user->email],[
+
+                'name' => $user->name,
+
+                'google_id'=> $user->id,
+
+                'password' => encrypt('123456dummy')
+
+            ]);
+
+        Auth::login($newUser);
+
+        return redirect()->intended('home');
+
+    }
+
+} catch (Exception $e) {
+
+    dd($e->getMessage());
+
+}
     }
 }
+
+
+
+
